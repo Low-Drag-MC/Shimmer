d4b3f5f618c026c156117b263d9058bd8f2f6d71
zomb <zomb_676@qq.com>
2022-05-31 12:06:37 +0800

    replace some @Inject with @Redirect to avoid the usage of LocalCapture

--- src/main/java/com/lowdragmc/shimmer/core/mixins/ParticleEngineMixin.java
+++ src/main/java/com/lowdragmc/shimmer/core/mixins/ParticleEngineMixin.java
@@ -1,10 +1,12 @@
 package com.lowdragmc.shimmer.core.mixins;
 
-import com.lowdragmc.shimmer.client.postprocessing.PostProcessing;
-import com.lowdragmc.shimmer.client.postprocessing.PostParticle;
 import com.lowdragmc.shimmer.client.postprocessing.IPostParticleType;
+import com.lowdragmc.shimmer.client.postprocessing.PostParticle;
+import com.lowdragmc.shimmer.client.postprocessing.PostProcessing;
 import com.lowdragmc.shimmer.core.IParticleEngine;
+import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.Particle;
@@ -13,17 +15,15 @@ import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.core.particles.ParticleOptions;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import javax.annotation.Nullable;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Queue;
 
@@ -40,6 +40,8 @@ public abstract class ParticleEngineMixin implements IParticleEngine {
 
     @Shadow @Final private Map<ParticleRenderType, Queue<Particle>> particles;
 
+    @Shadow @Final private TextureManager textureManager;
+
     @Nullable
     public Particle createPostParticle(PostProcessing postProcessing, ParticleOptions pParticleData, double pX, double pY, double pZ, double pXSpeed, double pYSpeed, double pZSpeed) {
         Particle particle = makeParticle(pParticleData, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed);
@@ -52,18 +54,10 @@ public abstract class ParticleEngineMixin implements IParticleEngine {
         }
     }
 
-    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V",
+    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V",
             at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/client/particle/ParticleRenderType;begin(Lcom/mojang/blaze3d/vertex/BufferBuilder;Lnet/minecraft/client/renderer/texture/TextureManager;)V"),
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    private void injectRenderPre(PoseStack pMatrixStack,
-                                 MultiBufferSource.BufferSource pBuffer,
-                                 LightTexture pLightTexture,
-                                 Camera pActiveRenderInfo, float pPartialTicks,
-                                 Frustum clippingHelper, CallbackInfo ci,
-                                 PoseStack posestack,
-                                 Iterator var8,
-                                 ParticleRenderType particlerendertype) {
+                    target = "Lnet/minecraft/client/particle/ParticleRenderType;begin(Lcom/mojang/blaze3d/vertex/BufferBuilder;Lnet/minecraft/client/renderer/texture/TextureManager;)V"))
+    private void injectRenderPre(ParticleRenderType particlerendertype, BufferBuilder bufferBuilder, TextureManager textureManager){
         if (particlerendertype instanceof IPostParticleType && this.particles.get(particlerendertype).size() > 0) {
             PostProcessing postProcessing = ((IPostParticleType) particlerendertype).getPost();
             postProcessing.getPostTarget().bindWrite(false);
@@ -71,18 +65,10 @@ public abstract class ParticleEngineMixin implements IParticleEngine {
         }
     }
 
-    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/client/particle/ParticleRenderType;end(Lcom/mojang/blaze3d/vertex/Tesselator;)V",
-                    shift = At.Shift.AFTER),
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    private void injectRenderPost(PoseStack pMatrixStack,
-                                  MultiBufferSource.BufferSource pBuffer,
-                                  LightTexture pLightTexture,
-                                  Camera pActiveRenderInfo, float pPartialTicks,
-                                  Frustum clippingHelper, CallbackInfo ci,
-                                  PoseStack posestack, Iterator var8,
-                                  ParticleRenderType particlerendertype) {
+    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V",
+        at = @At(value = "INVOKE",
+                target = "Lnet/minecraft/client/particle/ParticleRenderType;end(Lcom/mojang/blaze3d/vertex/Tesselator;)V"))
+    private void injectRenderPost(ParticleRenderType particlerendertype, Tesselator tesselator){
         if (particlerendertype instanceof IPostParticleType) {
             Minecraft.getInstance().getMainRenderTarget().bindWrite(false);
         }
--- src/main/java/com/lowdragmc/shimmer/core/mixins/RebuildTaskMixin.java
+++ src/main/java/com/lowdragmc/shimmer/core/mixins/RebuildTaskMixin.java
@@ -4,12 +4,9 @@ import com.google.common.collect.ImmutableList;
 import com.lowdragmc.shimmer.client.light.ColorPointLight;
 import com.lowdragmc.shimmer.client.light.LightManager;
 import com.lowdragmc.shimmer.core.IRenderChunk;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.ChunkBufferBuilderPack;
-import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
 import net.minecraft.client.renderer.chunk.RenderChunkRegion;
-import net.minecraft.client.renderer.chunk.VisGraph;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,11 +16,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
-import java.util.Random;
 import java.util.Set;
 
 /**
@@ -31,36 +26,25 @@ import java.util.Set;
  * @date 2022/05/02
  * @implNote RebuildTaskMixin, used to compile and save light info to the chunk.
  */
-@Mixin(targets = { "net/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask" })
+@Mixin(targets = {"net/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask"})
 public abstract class RebuildTaskMixin {
     @Shadow @Final ChunkRenderDispatcher.RenderChunk this$1;
     ImmutableList.Builder<ColorPointLight> lights;
 
-    @Inject(method = "compile",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask;getModelData(Lnet/minecraft/core/BlockPos;)Lnet/minecraftforge/client/model/data/IModelData;"),
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    private void injectCompile(float pX, float pY, float pZ,
-                               ChunkRenderDispatcher.CompiledChunk pCompiledChunk,
-                               ChunkBufferBuilderPack pBuffers,
-                               CallbackInfoReturnable<Set<BlockEntity>> cir,
-                               int i, BlockPos blockpos, BlockPos blockpos1,
-                               VisGraph visgraph, Set set,
-                               RenderChunkRegion renderchunkregion,
-                               PoseStack posestack, Random random,
-                               BlockRenderDispatcher blockrenderdispatcher,
-                               Iterator var15,
-                               BlockPos blockpos2,
-                               BlockState blockstate,
-                               BlockState blockstate1,
-                               FluidState fluidstate) {
+    @Redirect(method = "compile",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/chunk/RenderChunkRegion;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;",
+                    ordinal = 0))
+    private BlockState injectCompile(RenderChunkRegion instance, BlockPos pPos) {
+        BlockState blockstate = instance.getBlockState(pPos);
+        FluidState fluidstate = blockstate.getFluidState();
         if (LightManager.INSTANCE.isBlockHasLight(blockstate.getBlock(), fluidstate)) {
-            ColorPointLight light = LightManager.INSTANCE.getBlockStateLight(blockpos2, blockstate, fluidstate);
+            ColorPointLight light = LightManager.INSTANCE.getBlockStateLight(pPos, blockstate, fluidstate);
             if (light != null) {
                 lights.add(light);
             }
         }
+        return blockstate;
     }
 
     @Inject(method = "compile", at = @At(value = "HEAD"))
