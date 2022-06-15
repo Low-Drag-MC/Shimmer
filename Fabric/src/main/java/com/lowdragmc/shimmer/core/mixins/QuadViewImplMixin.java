0cb78c07d949209aec985f2ef923208ea962b206
Yefancy <yefancy@foxmail.com>
2022-06-15 19:34:07 +0800

    fix fabric block bloom

---
+++ Fabric/src/main/java/com/lowdragmc/shimmer/core/mixins/QuadViewImplMixin.java
@@ -0,0 +1,33 @@
+package com.lowdragmc.shimmer.core.mixins;
+
+import com.lowdragmc.shimmer.core.IQuadViewImpl;
+import net.fabricmc.fabric.impl.client.indigo.renderer.mesh.QuadViewImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author KilaBash
+ * @date 2022/06/15
+ */
+@Mixin(QuadViewImpl.class)
+public abstract class QuadViewImplMixin implements IQuadViewImpl {
+
+    private boolean isBloom;
+
+    @Inject(method = "lightmap", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    public void injectBaked(int vertexIndex, CallbackInfoReturnable<Integer> cir) {
+        if (isBloom) {
+            cir.setReturnValue(0x1000100);
+        }
+    }
+
+    public boolean isBloom() {
+        return isBloom;
+    }
+
+    public void setBloom(boolean isBloom) {
+        this.isBloom = isBloom;
+    }
+}
