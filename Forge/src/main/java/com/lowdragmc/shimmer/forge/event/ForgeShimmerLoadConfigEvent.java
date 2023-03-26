4ba11d9d488096a8e68bcbd13fe78a9b441cda6e
agnor99 <kreader100@gmail.com>
2023-03-26 23:47:09 +0200

    Fixes ForgeEvents not firing and adds more places to load Configurations from
    (cherry picked from commit a7be84f95c6f1dc6623ac4a51a434e900765a059)

--- Forge/src/main/java/com/lowdragmc/shimmer/forge/event/ForgeShimmerLoadConfigEvent.java
+++ Forge/src/main/java/com/lowdragmc/shimmer/forge/event/ForgeShimmerLoadConfigEvent.java
@@ -2,9 +2,10 @@ package com.lowdragmc.shimmer.forge.event;
 
 import com.lowdragmc.shimmer.event.ShimmerLoadConfigEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.IModBusEvent;
 
 
-public class ForgeShimmerLoadConfigEvent extends Event {
+public class ForgeShimmerLoadConfigEvent extends Event implements IModBusEvent {
     public final ShimmerLoadConfigEvent event;
 
     public ForgeShimmerLoadConfigEvent(ShimmerLoadConfigEvent event) {
